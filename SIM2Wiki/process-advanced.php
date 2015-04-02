881d0a2c260cbbdea7c373f87426d969b2fc1002
James Bolton <neojames11@gmail.com>
2015-04-02 01:11:48 +0100

    A really long time

--- SIM2Wiki/process-advanced.php
+++ SIM2Wiki/process-advanced.php
@@ -35,12 +35,14 @@ See http://neojames.me/ for more information
 	}
 	
 	$additionalPeopleRaw = isset($_POST['additionalNames'])?$_POST['additionalNames']:"";
-	$additionalPeople = explode("\n", str_replace("\r", "", $_POST['additionalNames']));
+	$additionalPeopleNoColon = explode("\n", str_replace("\r", "", $_POST['additionalNames']));
 	
-	$replacesAdditionalPeople = array_walk($additionalPeople, function(&$item){ "'''" . $item . "''':";});
+	foreach($array as &$row){
+		$replacesAdditionalPeople = $namesdb[] = $row . ":";
+		$additionalPeople[] = "'''" . $row . "''':";
+	}
 
 	$replaces = array_merge($replacesdb, $replacesAdditionalPeople);
-	
 	$names = array_merge($namesdb, $additionalPeople);
 	
 	$sim = $_POST['sim']; //Fetches SIM from text box.
