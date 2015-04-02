eeb02a420cf167f6c9abdefca505675217bcd117
James Bolton <neojames11@gmail.com>
2015-04-02 03:54:00 +0100

    Hotfix for version

--- SIM2Wiki/attribution.php
+++ SIM2Wiki/attribution.php
@@ -3,5 +3,5 @@
 //Distrubuted under the LGPL
 //See http://neojames.me/ for more information
 
-echo '<p><a href="https://github.com/neojames/SIM2Wiki">SIM2Wiki</a> is distributed under the <a href="http://www.gnu.org/licenses/lgpl.html">LGPL</a>. Version SIM2Wiki Version <?php $contents = file("version"); $string = implode($contents); print $string; ?> <b>Powered by <a href="http://en.wikipedia.org/wiki/Zefram_Cochrane">Zefram Cochrane</a>.</b><p>';
+echo '<p><a href="https://github.com/neojames/SIM2Wiki">SIM2Wiki</a> is distributed under the <a href="http://www.gnu.org/licenses/lgpl.html">LGPL</a>. SIM2Wiki Version ' . $string . '.<b> Powered by <a href="http://en.wikipedia.org/wiki/Zefram_Cochrane">Zefram Cochrane</a>.</b><p>';
 ?>
