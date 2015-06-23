46abb6ea9072d3dbcfd2a50f4401258900030f8f
James Bolton <neojames11@gmail.com>
2015-06-23 16:47:26 +0100

    Converted all JS to jQuery

--- SIM2Wiki/index.php
+++ SIM2Wiki/index.php
@@ -13,14 +13,15 @@ See http://neojames.me/ for more information
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title>SIM2Wiki</title>
 		<script type="text/javascript" src="js/general.js"></script>
+        <script type="text/javascript" src="js/index.js"></script>
 	</head>
 	
 	<body>
 		<h2>SIM2Wiki</h2>
 		
 		<form id="form" action="process-advanced.php" onsubmit="return OnSubmitForm();" method="post" style="width:800px">
-			<input type="radio" name="operation" value="1"  onclick="People();" checked>Advanced Mode
-			<input type="radio" name="operation" value="2" onclick="People();">Basic Mode
+			<input type="radio" name="people" value="0" checked>Advanced Mode
+			<input type="radio" name="people" value="1">Basic Mode
    			<br><br>
 			<textarea name="sim" rows="30" cols="80" style="-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;width:100%;">Enter SIM here.</textarea>
 			<br><br>
@@ -28,7 +29,7 @@ See http://neojames.me/ for more information
 			<br><br>
 			<div id="People" style="border:1px solid black;width:100%;">
 				<h4>People</h4>
-				<p>Here you can provide a list of names to be processed. Names should be entred in the format used by your sims, E.G the Discovery-C used to require names to be in caps, so the charicter James should be entred as JAMES. Also, plese don't include the colon (:) otherwise the paraser won't rcongnise it.</p>
+				<p>Here you can provide a list of names to be processed. Names should be entred in the format used by your sims, E.G the Discovery-C used to require names to be in caps, so the charicter James should be entred as JAMES. Also, please don't include the colon (:) otherwise the paraser won't rcongnise it.</p>
 				
 				<h5>Example Input <a href="#" class="toggleVisibility" data-target=".hideable">Show/Hide</a></h5>
 				<div class="hideable" style="display:none;">
--- SIM2Wiki/js/general.js
+++ SIM2Wiki/js/general.js
@@ -1,58 +1,9 @@
 //SIM2Wiki by James Bolton.
 //Distrubuted under the LGPL
 //See http://neojames.me/ for more information
-
-function OnSubmitForm(){
-	if(document.getElementById('form').operation[0].checked == true){
-		document.getElementById('form').action ="process-advanced.php";
-		}
-					
-	else
-				
-	if(document.getElementById('form').operation[1].checked == true){
-		document.getElementById('form').action ="process-basic.php";
-	}
-				
-	return true;
-}
 			
-function People(){
-	if(document.getElementById('form').operation[0].checked == true){
-		document.getElementById('People').style.display = 'block';
-}
-				
-	else
-				
-	if(document.getElementById('form').operation[1].checked == true){
-		document.getElementById('People').style.display = 'none';
-	}
-}
-
 $(document).ready(function(){
     $('.toggleVisibility').click(function () {
-        $($(this).data('target')).toggle();
+        $($(this).data('target')).toggle('slow');
     });
 });
-
-//function toggleVisibility(obj){
-//	var el = document.getElementById(obj);
-
-//	if(el.style.display == 'block') {
-//		el.style.display = 'none';
-//	}
-
-//	else{
-//		el.style.display = 'block';
-//	}
-//}
-	
-//Load schedular	
-		
-if(window.addEventListener) { // Mozilla, Netscape, Firefox
-    window.addEventListener('load', People, false);
-
-} 
-			
-else if(window.attachEvent) { // IE
-	window.attachEvent('onload', People);
-}
