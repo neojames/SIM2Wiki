2ecf32d015f198e28ed392e48b0e7222a1de4267
James Bolton <james@neojames.me>
2015-07-18 04:01:03 +0100

    Forgot to include assets

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+﻿//SIM2Wiki by James Bolton.
+//Distributed under the LGPL
+//See http://neojames.me/ for more information
+
+$(document).ready(function () {
+	$('input[name="people"]').change(function () {
+		if ($('input[name="people"][value="0"]').is(":checked") === true) {
+			$('#form').attr('action', "process-advanced.php");
+			$('#People').show('slow');
+			$('#Templates').show('slow');
+		}
+
+		else
+
+		if ($('input[name="people"][value="1"]').is(":checked") === true) {
+			$('#form').attr('action', "process-basic.php");
+			$('#People').hide('slow');
+			$('#Templates').hide('slow');
+		}
+	});
+
+	$('input[name="templates"]').change(function () {
+		if 
+	}
+});
